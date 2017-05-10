a75b06c9def4073355f2953d08fab5513106c00c
Tero @RC-Thoughts.com <tero@rc-thoughts.com>
2017-05-10 20:23:19 +0300

    Revert "v.1.1 Fixed wrong display of HDOP"
    This reverts commit 9b4dab0470c9463eb681e2e7b47490a5458556ea.
    
    # Conflicts:
    #	Arduino/RCT-GPS_Sensor/RCT-GPS_Sensor.hex

--- Arduino/RCT-GPS_Sensor/RCT-GPS_Sensor.ino
+++ Arduino/RCT-GPS_Sensor/RCT-GPS_Sensor.ino
@@ -98,7 +98,7 @@ JETISENSOR_CONST sensorsEUext[] PROGMEM =
   { ID_HEADING,     "Heading",    "\xB0",       JetiSensor::TYPE_14b, 0 },
   { ID_COURSE,      "Course",     "\xB0",       JetiSensor::TYPE_14b, 0 },
   { ID_SATS,        "Satellites", " ",          JetiSensor::TYPE_14b, 0 },
-  { ID_HDOP,        "HDOP",       " ",          JetiSensor::TYPE_14b, 2 },
+  { ID_HDOP,        "HDOP",       " ",          JetiSensor::TYPE_14b, 0 },
   { 0 }
 };
 
@@ -123,7 +123,7 @@ JETISENSOR_CONST sensorsUSext[] PROGMEM =
   { ID_HEADING,     "Heading",    "\xB0",       JetiSensor::TYPE_14b, 0 },
   { ID_COURSE,      "Course",     "\xB0",       JetiSensor::TYPE_14b, 0 },
   { ID_SATS,        "Satellites", " ",          JetiSensor::TYPE_14b, 0 },
-  { ID_HDOP,        "HDOP",       " ",          JetiSensor::TYPE_14b, 2 },
+  { ID_HDOP,        "HDOP",       " ",          JetiSensor::TYPE_14b, 0 },
   { 0 }
 };
 
